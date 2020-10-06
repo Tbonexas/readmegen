614d4e8ae1a2c2eebb88cc0298a6e5a1b6e895f7
Todd Murdoch <TheClash090@gmail.com>
2020-10-05 20:19:11 -0700

    working on bugs with Project Title.

---
+++ index.js
@@ -0,0 +1,105 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require('fs');
+const path = require('path');
+async function main(){
+    console.log(`starting`);
+    const userResponse = await inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your GitHub user name?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your Project Title?",
+            name: "projectTitle"
+        },
+        {
+            type: "input",
+            message: "Provide detail description",
+            name: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+            name: "installationProcess"
+        },
+        {
+            type: "input",
+            message: "Provide instructions for use.",
+            name: "instruction"
+        },
+        {
+            type: "input",
+            message: "Provide instructions examples for use.",
+            name: "instructionExample"
+        },
+        {
+            type: "input",
+            message: "provide License name ",
+            name: "licenseName"
+        },
+        {
+            type: "input",
+            message: "provide License url ",
+            name: "licenseUrl"
+        },
+       
+        ]);
+
+
+        console.log(`starting`);
+        console.log(userResponse);
+        const gitUsername = userResponse.username;
+        const email = userResponse.email;
+        const projectTitle = userResponse.projectTitle;
+        const projectDescription = userResponse.projectDescription;
+        const installationProcess = userResponse.installationProcess;
+        const instruction = userResponse.instruction;
+        const instructionExample = userResponse.instructionExample;
+        const licenseName = userResponse.licenseName;
+        const licenseUrl = userResponse.licenseUrl;
+            // fetching data from git
+            // user
+        const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+        const gitData = gitResponse.data;
+        const gitName = gitData.login;
+        const gitEmail = gitData.email;
+        const gitlocation = gitData.location;
+        const gitUrl = gitData.html_url;
+  
+        }
+        var result = (`
+# ${projectTitle} 
+${projectDescription}
+\n* [Installation](#Installation)
+\n* [Instructions](#Instructions)
+\n* [License](#License)
+\n* [Author](#Author)
+## Installation
+${installationProcess}
+## Instructions
+${instruction}
+\`\`\`
+${instructionExample}
+\`\`\`
+## License 
+This project is licensed under the ${licenseName} - see the ${licenseUrl} file for details
+## Author 
+\n![ProfileImage](${gitProfileImage})
+\n**${gitName}**
+\nEmail: ${gitEmail}
+\nLocation:${gitlocation}
+\nGitHub: ${gitUrl}
+`)
+var writeResult = fs.writeFileSync(path.join(__dirname, '../readmegen', 'readMe.md'), result )
+console.log("file generated....")
+    
+main();
